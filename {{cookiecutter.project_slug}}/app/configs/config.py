f001bb34333870b411d0b8ccab6cb601033e3f56
yvedengw <yvedengw@gmail.com>
2021-05-13 14:09:31 +0200

    Add configs

--- {{cookiecutter.project_slug}}/app/configs/config.py
+++ {{cookiecutter.project_slug}}/app/configs/config.py
@@ -1,9 +1,21 @@
-postgres_settings = {
-    "DB_HOST": "localhost",
-    "DB_NAME": "appdb",
-    "DB_USER": "appuser",
-}
+import configparser
+import os
+import logging
 
-redis_settings = {}
 
-auth_settings = {"PRIVATE_KEY": "", "PUBLIC_KEY": ""}
+def get_config() -> configparser.ConfigParser:
+    logger = logging.getLogger(__name__)
+    environment = os.getenv("ENV", "dev")
+    logger.info(f"We are running in the {environment} environment")
+
+    read_file = __config_filename(environment)
+    config = configparser.ConfigParser()
+    config.read(read_file)
+    logger.info(f"Reading config from file {read_file}")
+
+    print(config)
+    return config
+
+
+def __config_filename(env: str) -> str:
+    return f"config_{env}"
--- {{cookiecutter.project_slug}}/app/server.py
+++ {{cookiecutter.project_slug}}/app/server.py
@@ -2,12 +2,14 @@ from sanic import Sanic
 from sanic.response import text
 import multiprocessing
 
+
 from auth.auth import protected
 from auth.login import login
-from configs.config import auth_settings
+from configs.config import get_config
 
 app = Sanic("{{cookiecutter.app_name}}")
-app.config.update(auth_settings)
+
+app.config = get_config()
 
 app.blueprint(login)
 
